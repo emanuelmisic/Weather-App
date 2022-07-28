beb5e850d613b485af5568168c7ca4c4465defdf
emanuelmisic <emanuelmisic64@gmail.com>
2022-07-28 22:07:16 +0200

    Define all elements

---
+++ script.js
@@ -0,0 +1,26 @@
+// Define elements
+const searchBar = document.querySelector('#searchBar');
+const searchBtn = document.querySelector('#searchBtn');
+
+const location = document.querySelector('#location');
+
+const date = document.querySelector('#date');
+const weather = document.querySelector('#weatherStateMain');
+const temperature = document.querySelector('#temperatureMain');
+const wind = document.querySelector('#windSpeedMain');
+
+const weather1 = document.querySelector('#weatherStateDay1');
+const temp1 = document.querySelector('#temperatureDay1');
+const humidity1 = document.querySelector('#humidityDay1');
+const weather2 = document.querySelector('#weatherStateDay2');
+const temp2 = document.querySelector('#temperatureDay2');
+const humidity2 = document.querySelector('#humidityDay2');
+const weather3 = document.querySelector('#weatherStateDay3');
+const temp3 = document.querySelector('#temperatureDay3');
+const humidity3 = document.querySelector('#humidityDay3');
+const weather4 = document.querySelector('#weatherStateDay4');
+const temp4 = document.querySelector('#temperatureDay4');
+const humidity4 = document.querySelector('#humidityDay4');
+const weather5 = document.querySelector('#weatherStateDay5');
+const temp5 = document.querySelector('#temperatureDay5');
+const humidity5 = document.querySelector('#humidityDay5');
