e8ce6a57490e74e3c037987613ddc136ef7cfab4
Emanuel <emanuelmisic64@gmail.com>
2023-01-29 19:25:46 +0100

    add fetch functions

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,39 @@
 import "../styles/SearchBar.css";
+import axios from "axios";
+
+let lat = 0;
+let lon = 0;
+
+async function fetchCityGeolocation() {
+  try {
+    const res = await axios({ method: "get", url: `http://api.openweathermap.org/geo/1.0/direct?q=Zagreb&limit=1&appid=${process.env.REACT_APP_API_KEY}` });
+    lat = res.data[0].lat;
+    lon = res.data[0].lon;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function fetchWeather() {
+  try {
+    await fetchCityGeolocation();
+    const res = await axios({
+      method: "get",
+      url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`,
+    });
+    console.log(res.data);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 function SearchBar() {
   return (
     <div className="container">
       <input type="text" className="input" />
-      <button className="button">Search</button>
+      <button className="button" onClick={fetchWeather}>
+        Search
+      </button>
     </div>
   );
 }
