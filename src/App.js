e4d76df35b1db6456a5f70caa73841ca92c82507
Emanuel <emanuelmisic64@gmail.com>
2023-01-29 20:19:51 +0100

    add WatherInfo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import SearchBar from "./components/SearchBar";
+import WeatherInfo from "./components/WeatherInfo";
 
 function App() {
   return (
     <div className="app-dark">
       <SearchBar />
+      <WeatherInfo />
     </div>
   );
 }
---
+++ src/components/WeatherInfo.js
@@ -0,0 +1,17 @@
+import "../styles/WeatherInfo.css";
+
+function WeatherInfo() {
+  return (
+    <div className="main-container">
+      <div className="today">
+        <img src="" width="100em" />
+        <p>Temp</p>
+        <p>Feels like</p>
+        <p>Wind speed</p>
+        <p>Visibility</p>
+      </div>
+    </div>
+  );
+}
+
+export default WeatherInfo;
